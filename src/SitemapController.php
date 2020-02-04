8fecbf051ed3d39f502476d67eda91746e3566bf
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-02-04 17:23:51 +0100

    v0.0.3 changes src/SitemapController.php

--- src/SitemapController.php
+++ src/SitemapController.php
@@ -9,19 +9,30 @@ class SitemapController
 	 * Sitemap route action
 	 *
 	 * @param  string $file
-	 * @return void
+	 * @return mixed
 	 */
-	public function sitemap($file): void
+	public function sitemap($file)
 	{
+		$file = config('sitemap.save_path') . "/{$file}";
 
+		if (dirname(realpath($file)) !== config('sitemap.save_path')) {
+
+			return abort(403);
+
+		} elseif (file_exists($file) === false) {
+
+			return abort(404);
+		}
+
+		return response(file_get_contents($file))->header('content-type', 'text/xml; charset=utf-8');
 	}
 
 	/**
 	 * Robots.txt route action
 	 *
-	 * @return void
+	 * @return mixed
 	 */
-	public function robots(): void
+	public function robots()
 	{
 
 	}
