7b9794af416becc00f84380e433753d1b7aee3ba
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-02-04 17:23:28 +0100

    init src/Traits/SitemapGeneratorTrait.php

---
+++ src/Traits/SitemapGeneratorTrait.php
@@ -0,0 +1,46 @@
+<?php
+namespace IMW\LaravelSitemap\Traits;
+
+use IMW\LaravelSitemap\Map;
+
+trait SitemapGeneratorTrait
+{
+
+	# Images in sitemap file
+	protected $images = false;
+
+	# Videos in sitemap file
+	protected $videos = false;
+
+	# Sitemap name
+	protected $name = 'example.xml';
+
+	/** @var Map */
+	protected $map;
+
+
+	protected function initSitemap()
+	{
+		$this->map = new Map($this->name, $this->images, $this->videos);
+	}
+
+	/**
+	 * Get map object
+	 *
+	 * @return Map
+	 */
+	public function getMap(): Map
+	{
+		return $this->map;
+	}
+
+	/**
+	 * Get sitemap name
+	 * @return string
+	 */
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+}
